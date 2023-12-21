03ce2da33d64ab0c20438cfedd920d5cf1f43ce4
bob <zq188140725@gmail.com>
2023-12-21 15:09:35 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="flex">
+    <button>Hello world</button>
+    <input type="text" />
+  </view>
+</template>
+
+<script>
+import { ref } from "vue";
+import "./index.css";
+
+export default {
+  setup() {
+    const msg = ref("Hello world");
+    return {
+      msg,
+    };
+  },
+};
+</script>
+<style>
+.flex {
+  display: flex;
+}
+</style>
